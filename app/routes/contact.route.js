0d325a6fcee634d37682f2fc9b0a6caf4182645c
Phan Tấn Lộc <ginga550504@gmail.com>
2023-02-14 07:55:59 +0700

    Dinh nghia cac route cho tai nguyen Contact

---
+++ app/routes/contact.route.js
@@ -0,0 +1,20 @@
+const { Router } = require("express");
+const config = require("../controllers/contact.controller");
+
+const {
+  findAll,
+  create,
+  deleteAll,
+  findAllFavorite,
+  findOne,
+  update,
+  delete: deleteOne,
+} = config;
+
+const router = Router();
+
+router.route("/").get(findAll).post(create).delete(deleteAll);
+
+router.route("/favorite").get(findAllFavorite);
+
+router.route("/:id").get(findOne).put(update).delete(deleteOne);

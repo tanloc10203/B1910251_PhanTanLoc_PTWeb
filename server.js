62a2188d3efe5dae695d179503ac04d258aaea2d
Phan Tấn Lộc <ginga550504@gmail.com>
2023-02-21 08:59:42 +0700

    Cai dat cac handler truy xua CSdl

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const app = require("./app");
 const config = require("./app/config");
+const MongoDB = require("./app/utils/mongodb.util");
 
-const PORT = config.app.port;
+(async () => {
+  try {
+    await MongoDB.connect(config.db.uri);
 
-app.listen(PORT, () => {
-  console.log(`SERVER IS RUNNING ON PORT ${PORT}`);
-});
+    console.log("Connected to the database!");
+
+    const PORT = config.app.port;
+
+    app.listen(PORT, () => {
+      console.log(`SERVER IS RUNNING ON PORT ${PORT}`);
+    });
+  } catch (error) {
+    console.log("Cannot connect to the database!", error);
+    process.exit("Exit server");
+  }
+})();
